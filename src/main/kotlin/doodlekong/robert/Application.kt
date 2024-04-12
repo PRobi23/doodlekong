174003715f59fa6879aad679f9996c08f2c30dde
robertpapp <robert.papp@icapps.com>
2024-04-12 23:07:32 +0200

    Initial commit

---
+++ src/main/kotlin/doodlekong/robert/Application.kt
@@ -0,0 +1,18 @@
+package doodlekong.robert
+
+import doodlekong.robert.plugins.*
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    configureMonitoring()
+    configureSockets()
+    configureSerialization()
+    configureRouting()
+}
